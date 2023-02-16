5b0174a0802c89ada6e0080dae57a86ddc84e3ec
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-02-15 21:50:59 -0500

    Add example script showing how to call ghg-processor.qmd

---
+++ example.R
@@ -0,0 +1,23 @@
+# Example of how to call the ghg-processor.qmd file to do
+# initial data processing, before proceeding with analysis
+# BBL 2023-02-15
+
+library(quarto)
+
+# Run the processor file as if you click the "render" button
+quarto_render("ghg-processor.qmd")
+# This script could now read in the results from the output/ folder
+
+# Run it but don't generate individual graphs (which is a slow step)
+quarto_render("ghg-processor.qmd",
+              execute_params = list(SAVE_RESULTS_FIGURES = 0))
+
+# Run with a different dead band default
+quarto_render("ghg-processor.qmd",
+              execute_params = list(DEAD_BAND_SEC = 30))
+
+# If your data or metadata are someplace else, you can override the
+# Quarto file location defaults
+# This will not work unless you have this folder on your computer!
+quarto_render("ghg-processor.qmd",
+              execute_params = list(data_folder = "/Users/bpbond/mydata"))
